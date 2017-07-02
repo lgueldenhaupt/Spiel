a6b82ffc2635b833f4f26a2902f957e8f3db286e
lgueldenhaupt <lgueldenhaupt@gmail.com>
2017-07-02 14:49:25 +0200

    Added Fonts and Changed Camera
    Changed Camera to smoothly follow the player.
    Added Roboto fonts

---
+++ FutureGame/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform cam;
+    public Transform player;
+    public float vertSpeed = 20f;
+    public float horiSpeed = 20f;
+    public float yaw = 0f;
+    public float pitch = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        cam.LookAt(player);
+        Vector3 toMove = new Vector3(Input.GetAxis("Mouse X") * 0.5f, Input.GetAxis("Mouse Y") * 0.5f, 0);
+        cam.Translate(toMove);
+	}
+}
---
+++ FutureGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ResumeGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //Closes the application completely
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
